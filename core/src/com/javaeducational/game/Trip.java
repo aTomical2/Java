6982c9c6eb591a57e993a76b6907d4f2e354ec98
DAVIDWOOD5 <david.woods3@ucdconnect.ie>
2024-04-02 17:58:20 +0100

    Initial commit

---
+++ core/src/com/javaeducational/game/Trip.java
@@ -0,0 +1,23 @@
+package com.javaeducational.game;
+
+import com.javaeducational.game.tools.Route;
+
+public class Trip {
+    public void travel() {
+        System.out.println("Player is now traveling... ");
+    }
+    public Trip() {
+
+    }
+    public getCurrentLocation(){
+
+    }
+    public startTrip(){
+
+    }
+    public endTrip(){
+
+    }
+
+}
+
