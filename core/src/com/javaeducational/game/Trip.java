ab3935fb7839e58efcdef1fa8edbcbe5dde6039f
DAVIDWOOD5 <david.woods3@ucdconnect.ie>
2024-04-04 08:03:51 +0100

    Added class constructors, + methods for getting location and starting trip

--- core/src/com/javaeducational/game/Trip.java
+++ core/src/com/javaeducational/game/Trip.java
@@ -3,16 +3,27 @@ package com.javaeducational.game;
 import com.javaeducational.game.tools.Route;
 
 public class Trip {
-    public Trip() {
+    // Setting the instance variables for the class
+    private Route route;
+    private int currentLocation;
+    private boolean tripStarted;
 
+    public Trip(Route route) {
+        this.route= route;
+        this.currentLocation = route.getStartPoint();
+        this.tripStarted = false;
     }
-//    public getCurrentLocation(){
-//
-//    }
-//    public startTrip(){
-//
-//    }
-//    public void travel() {
+
+    public int getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void startTrip() {
+        System.out.println("The trip has started!");
+        tripStarted = true;
+    }
+
+    //    public void travel() {
 //        System.out.println("Player is now traveling... ");
 //    }
 //    public endTrip(){
