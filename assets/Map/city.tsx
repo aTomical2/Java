f7da1d1a21c7b24b87fa1a1ce7090e02ae89c9cc
aTomical2 <tom.gibson@ucdconnect.ie>
2024-04-05 11:55:19 +0100

    Removed redundant tilesets, new tilesets for map actual

--- assets/Map/city.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="city1tileset" tilewidth="8" tileheight="8" tilecount="1312" columns="32">
- <image source="Images/citytilesetimage.gif" width="256" height="328"/>
-</tileset>
---
+++ assets/Map/citytileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="citytileset2" tilewidth="8" tileheight="8" tilecount="3072" columns="64">
+ <image source="Images/citytileset2image.png" width="512" height="384"/>
+</tileset>
